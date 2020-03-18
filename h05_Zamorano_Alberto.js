a3e8599a09d8833fa839331c44e826e500ec7274
Alberto Zamorano <azamor16@asu.edu>
2020-03-17 23:49:06 -0700

    Broken

---
+++ h05_Zamorano_Alberto.js
@@ -0,0 +1,12 @@
+var startingAge = document.getElementById("startingAge").value;
+var retirmentAge = document.getElementById("retirmentAge").value; 
+var salary = document.getElementById("salary").value; 
+var investment = document.getElementById("investment").value; 
+var raise = document.getElementById("raise").value; 
+var interstRate = document.getElementById("interstRate").value; 
+
+
+function printThings(){
+    console.log(startingAge);
+}
+
